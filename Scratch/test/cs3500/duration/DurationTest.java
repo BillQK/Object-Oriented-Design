5ce37c3949bf91dae8b84b942baf1761e0219979
BillQK <billngo47@gmail.com>
2022-01-25 16:57:42 -0500

    Added Test and implements Duration class

---
+++ Scratch/test/cs3500/duration/DurationTest.java
@@ -0,0 +1,26 @@
+package cs3500.duration;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+public class DurationTest {
+
+  @Test
+  public void testSort() {
+    List<Duration> lod = new ArrayList<>();
+    //.. populate list
+    Collections.sort(lod);
+  }
+  @Test
+  public void add() {
+  }
+
+  @Test
+  public void asHms() {
+  }
+}
