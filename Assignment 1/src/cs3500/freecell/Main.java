d1ff69c7d9bbdeeadcf61df54a570878e8731396
BillQK <billngo47@gmail.com>
2022-02-21 22:47:52 -0500

    Finished Main

---
+++ Assignment 1/src/cs3500/freecell/Main.java
@@ -0,0 +1,17 @@
+package cs3500.freecell;
+
+import java.io.InputStreamReader;
+
+import cs3500.freecell.controller.FreecellController;
+import cs3500.freecell.controller.SimpleFreecellController;
+import cs3500.freecell.model.Card;
+import cs3500.freecell.model.FreecellModel;
+import cs3500.freecell.model.SimpleFreecellModel;
+
+public class Main {
+  public static void main(String[] args) {
+    FreecellModel<Card> model = new SimpleFreecellModel();
+    FreecellController<Card> controller = new SimpleFreecellController<>(model,new InputStreamReader(System.in), System.out);
+    controller.playGame(model.getDeck(), 5,4, true);
+  }
+}
